2259087055d6c58d89637993958e96b6dd4cd70f
elias <99310710+bo-codes@users.noreply.github.com>
2023-04-29 20:06:57 -0700

    final demo version

--- src/OBJViewer/OBJViewer.js
+++ src/OBJViewer/OBJViewer.js
@@ -2,47 +2,37 @@ import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import './OBJViewer.css'
 
-const OBJViewer = ({OBJFile}) => {
+const OBJViewer = ({ OBJFile }) => {
   // CREATING REFERENCE TO THE DIV WHERE WE WILL SHOW OUR CANVAS
   const containerRef = useRef(null);
 
-  const [modelLoaded, setModelLoaded] = useState(false)
-
+  // -------------------------- THREE.JS CODE -------------------------- vv
   useEffect(() => {
-    // -------------- SCENE SETUP -------------- vv
     const scene = new THREE.Scene();
-    // ADDS MAIN XYZ(RGB) AXES TO SCENE
-    // scene.add(new THREE.AxesHelper(5));
-    //
-    //
-    // ------ CREATES CAMERA THAT WE WILL SEE THE SCENE THROUGH
+
+    // ------ CREATES CAMERA
     const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 2000);
-    // ------ SETS POSITION OF CAMERA(DEFAULT IS (0,0,0))
+
+    // ------ SETS POSITION OF CAMERA IN X,Y,Z(THE DEFAULT IS (0,0,0))
     camera.position.set(0, 0, -300);
+
     // ------ THIS CODE IS ALSO ACCOMPLISHING THE SAME AS THE CODE ABOVE
-    // camera.position.z = -400;
-    //
-    //
+    // camera.position.z = -300;
+
     // ------ CREATE AN AMBIENT LIGHT
     const ambLight = new THREE.AmbientLight(0x404040);
     // ------ ADD THE LIGHT TO THE SCENE
     scene.add(ambLight);
     // ------ CREATE A DIRECTIONAL LIGHT
     let light = new THREE.DirectionalLight();
-    light.position.set(10, 30, -30)
+    light.position.set(10, 30, -30);
     // ------ ADD THE LIGHT TO THE SCENE
     scene.add(light);
-    // ------ NOT SURE WHAT THIS DOES YET
     let helper = new THREE.DirectionalLightHelper(light);
     scene.add(helper);
-    // -------------- SCENE SETUP -------------- ^^
-    //
-    //
-    //
-    //
-    // -------------- CREATE ORBIT CONTROLS (to navigate the scene) -------------- vv
+
+    // ------ CREATES CONTROLS
     const controls = new OrbitControls(camera, containerRef.current);
     // MAKES THE PHYSICS HEAVIER/NICER FOR CONTROLS
     controls.enableDamping = true;
@@ -54,83 +44,51 @@ const OBJViewer = ({OBJFile}) => {
     controls.enablePan = false;
     // SETTING TO FALSE MAKES IT SO USER CAN'T ZOOM
     controls.enableZoom = false;
-    // -------------- CREATE ORBIT CONTROLS (to navigate the scene) -------------- ^^
-    //
-    //
-    //
-    //
-    // -------------------- RENDERER SETUP -------------------- vv
-    // ------ FUNCTION TO CALCULATE RENDER SIZE DEPENDING ON THE WIDTH OF OUR WINDOW(CHECKS IF WE ARE ON MOBILE OR NOT)
-    const renderSize = () => {
-      // if (window.innerWidth < 500) {
-      //   return 240;
-      // }
-      return 360;
-    };
+
     // ------ CREATES A NEW INSTANCE OF A RENDERER
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     // ------ SETS THE SIZE OF THE RENDERER TO THE VALUE OUR renderSize FUNCTION RETURNS
-    renderer.setSize(renderSize(), renderSize());
-    // -------------------- RENDERER SETUP -------------------- ^^
-    //
-    //
-    //
-    //
+    renderer.setSize(360, 360);
+
     // renderer HAS PROPERTY domElement. THIS HOLDS OUR CANVAS WITH OUR MODEL. WE ARE ATTACHING THAT TO THE DIV WHERE WE WANT TO SHOW IT BELOW IN OUR COMPONENT RETURN.
     containerRef.current.appendChild(renderer.domElement);
-    //
-    //
-    //
-    //
-    // -------------------- OBJECT LOADER SETUP -------------------- vv
+
     // ------ CREATES LOADER
     const objLoader = new OBJLoader();
     // ------ LOADS THE OBJ FILE
-    objLoader.load(OBJFile , (model) => {
+    objLoader.load(OBJFile, (model) => {
       // once the obj is loaded, add it to the scene
       scene.add(model);
       // once the obj is added to the scene, render the scene
       renderer.render(scene, camera);
-      setModelLoaded(true)
     });
-    // -------------------- OBJECT LOADER SETUP -------------------- ^^
-    //
-    //
-    //
-    //
-    // -------------------- ANIMATE FUNCTION -------------------- vv
+
     const animate = () => {
-      // ------ REQUEST THE ANIMATION
+      // ------ REQUEST RENDER OF DRAWINGS/ANIMATION
       requestAnimationFrame(animate);
       // ------ UPDATE THE CONTROLS
       controls.update();
-      // ------ THIS IS IF WE ARE USING THE LIGHT HELPER
-      helper.update();
       // ------ RENDER THE SCENE
       renderer.render(scene, camera);
     };
     // CALL THE ANIMATE FUNCTION
     animate();
-    // -------------------- ANIMATE FUNCTION -------------------- ^^
-    //
-    //
-    //
-    //
-    return () => {
-      // containerRef.current.removeChild(renderer.domElement);
-    };
-  },[OBJFile]);
+  }, [OBJFile]);
+  // -------------------------- THREE.JS CODE -------------------------- ^^
 
   return (
-    <div id="model-container">
-      <div
-        style={{ display: modelLoaded ? "block" : "none" }}
-        ref={containerRef}
-      ></div>
-      <div id='model-loading' style={{ display: !modelLoaded ? "flex" : "none" }}>
-        LOADING...
-      </div>
-      <div>BO_CODES X BLENDER_SUS</div>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div ref={containerRef}></div>
+      {/* THIS IS MY OWN TITLE YOU CAN INSERT YOUR OWN */}
+      <div style={{ margin: "10px" }}>BO_CODES X BLENDER_SUS</div>
     </div>
   );
 };
